1e90113c98dd99df3c5669c244602cf4c0d46700
Michał Mamiński <michal.maminski@gmail.com>
2019-01-20 15:43:28 +0100

    Add new turn verbs

--- js/AppPhrasalVerbs.js
+++ js/AppPhrasalVerbs.js
@@ -1,13 +1,12 @@
-export default [{
-        "en": "Turn on",
-        "pl": "Włączać, odkręcać"
-    },
-    {
-        "en": "Turn off",
-        "pl": "Wyłączać, zakręcać"
-    },
-    {
-        "en": "Turn up",
-        "pl": "Pojawiać się"
-    },
+export default [
+    { "en": "Turn on", "pl": "Włączać, odkręcać" },
+    { "en": "Turn off","pl": "Wyłączać, zakręcać" },
+    { "en": "Turn up", "pl": "Pojawiać się" },
+    { "en": "Turn down", "pl": "Ściszać, odrzucać np. ofertę" },
+    { "en": "Turn around", "pl": "Obracać się" },
+    { "en": "Turn over", "pl": "Przewracać na drugą stronę" },
+    { "en": "Turn out", "pl": "Okazywać się" },
+    { "en": "Turn back", "pl": "Zawracać" },
+    { "en": "Turn to sb", "pl": "Zwracać się do" },
+    { "en": "Turn into sth", "pl": "Przerobić, przerodzić się, przemieniać się" },
 ]
