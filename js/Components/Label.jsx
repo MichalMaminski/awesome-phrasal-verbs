053d5dcd834424e4bb6cc164828008de0e4c3dcd
Michał Mamiński <michal.maminski@partners.mbank.pl>
2019-01-26 11:29:30 +0100

    Changed labels for hidden word content

--- js/Components/Label.jsx
+++ js/Components/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Label extends React.Component {
     render() {
-        let placeholderText = this.props.isEn ? "Hidden content" : "Ukryta zawartość";
+        let placeholderText = this.props.isEn ? "<- tick to show" : "zaznacz, aby pokazać ->";
         let labelValue = this.props.showValue ? this.props.value : placeholderText;
         let placeholderColor = this.props.showValue ? "" : " text-secondary font-weight-light small";
         let styleClasses = this.props.isEn ? "custom-control-label" : "custom-control-label-reverted right-pulled";
