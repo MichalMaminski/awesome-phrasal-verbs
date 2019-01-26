e7f1ca300ffdd8f3447a38f7936dc902be7c8db2
Michał Mamiński <michal.maminski@partners.mbank.pl>
2019-01-26 13:05:44 +0100

    Make label 100% width to improve checking experience

--- js/Components/Label.jsx
+++ js/Components/Label.jsx
@@ -6,7 +6,7 @@ class Label extends React.Component {
         let placeholderText = this.props.isEn ? "<- tick to show" : "zaznacz, aby pokazać ->";
         let labelValue = this.props.showValue ? this.props.value : placeholderText;
         let placeholderColor = this.props.showValue ? "" : " text-secondary font-weight-light small";
-        let styleClasses = this.props.isEn ? "custom-control-label" : "custom-control-label-reverted right-pulled";
+        let styleClasses = this.props.isEn ? "custom-control-label w-100" : "custom-control-label-reverted right-pulled w-100";
         return (
             <label className={styleClasses + placeholderColor}
                 htmlFor={this.props.id} >
