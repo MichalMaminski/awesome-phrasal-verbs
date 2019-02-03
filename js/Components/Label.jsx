ab7e6face1af19308aee2dc1891e32ba356f1d4f
Michał Mamiński <michal.maminski@gmail.com>
2019-02-03 21:20:41 +0100

    Remove arrows from hidden value placeholders

--- js/Components/Label.jsx
+++ js/Components/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Label extends React.Component {
     render() {
-        let placeholderText = this.props.isEn ? "<- tick to show" : "zaznacz, aby pokazać ->";
+        let placeholderText = this.props.isEn ? "tick to show" : "zaznacz, aby pokazać";
         let labelValue = this.props.showValue ? this.props.value : placeholderText;
         let placeholderColor = this.props.showValue ? "" : " text-secondary font-weight-light small";
         let styleClasses = this.props.isEn ? "custom-control-label w-100" : "custom-control-label-reverted right-pulled w-100";
