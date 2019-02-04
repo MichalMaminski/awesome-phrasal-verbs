0da239e30bc25646f4e8e0b2dce999c5b9d64d00
Michał Mamiński <michal.maminski@gmail.com>
2019-02-04 22:12:30 +0100

    Switch to writeFile API

--- tools/make-site-view.js
+++ tools/make-site-view.js
@@ -4,8 +4,16 @@ const fs = require('fs')
 capture({
     url: 'https://michalmaminski.github.io/awesome-phrasal-verbs'
 }).then(screenshot => {
-    fs.writeFileSync(`${__dirname}/output/site-screenshot.png`, screenshot, { mode: 7, flag: "w+"});
-    console.log('Screenshot from site generated with success.');
+    console.log(`${__dirname}`);
+    console.log(fs.exists(`${__dirname}/output/`));
+    // fs.writeFile(`${__dirname}/output/site-screenshot.png`, screenshot, { mode: 7, flag: "w+"}, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         process.exit(-1);
+    //     }
+    //     console.log('Screenshot from site generated with success.');
+    //   });
+    
 }).catch((err) => {
     console.log(err);
     process.exit(-1);
