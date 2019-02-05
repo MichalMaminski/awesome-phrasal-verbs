3c2ba630852b7c3d5e02ba0dbcb32680d350a8b6
Michał Mamiński <michal.maminski@gmail.com>
2019-02-05 22:21:46 +0100

    Fix URL for trigger build for site screen CI job and remove redundant stringifying data

--- tools/run-site-image-maker-ci-build.js
+++ tools/run-site-image-maker-ci-build.js
@@ -2,19 +2,19 @@
 const axios = require('axios');
 console.log('Calling Travis...');
 
-axios.post(`https://api.travis-ci.org/repo/MichalMaminski/awesome-phrasal-verbs/requests`, {
+axios.post(`https://api.travis-ci.org/repo/MichalMaminski%2Fawesome-phrasal-verbs/requests`, {
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Travis-API-Version": "3",
         "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
     }
-}, JSON.stringify({
+}, {
     request: {
         message: `Trigger build at latest commit from readme-image-maker`,
         branch: 'readme-image-maker'
-    },
-}))
+    }
+})
     .then(() => {
         console.log("Triggered build of readme-image-maker");
     })
