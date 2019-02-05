f9e3ae09033a6b211b733de6c33544f8e2358822
Michał Mamiński <michal.maminski@gmail.com>
2019-02-05 22:27:50 +0100

    Swap post parameters in axios API - wrong data and config order

--- tools/run-site-image-maker-ci-build.js
+++ tools/run-site-image-maker-ci-build.js
@@ -3,18 +3,18 @@ const axios = require('axios');
 console.log('Calling Travis...');
 
 axios.post(`https://api.travis-ci.org/repo/MichalMaminski%2Fawesome-phrasal-verbs/requests`, {
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Travis-API-Version": "3",
-        "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
-    }
-}, {
     request: {
         message: `Trigger build at latest commit from readme-image-maker`,
         branch: 'readme-image-maker'
     }
-})
+}, {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Travis-API-Version": "3",
+            "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
+        }
+    })
     .then(() => {
         console.log("Triggered build of readme-image-maker");
     })
